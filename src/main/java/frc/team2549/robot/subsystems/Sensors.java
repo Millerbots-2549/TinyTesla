e265744ec187c8bdf99836a765bd320296166400
2549-Elitebook <millerbots@gmail.com>
2018-05-21 18:49:07 -0500

    added half speed option and fixed smartdashboard

--- src/main/java/frc/team2549/robot/subsystems/Sensors.java
+++ src/main/java/frc/team2549/robot/subsystems/Sensors.java
@@ -3,21 +3,20 @@ package frc.team2549.robot.subsystems;
 import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 import edu.wpi.first.wpilibj.BuiltInAccelerometer;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import com.analog.adis16448.frc.ADIS16448_IMU;
 
 import java.util.ArrayList;
 import java.util.List;
 
-// TODO: Get ADIS16448 working
 // TODO: Get Ultrasonic Sensors working (lots to do)
 
 public class Sensors extends Subsystem {
 
-    private ADXRS450_Gyro m_gyro;
-    private BuiltInAccelerometer m_accel;
+    private static final ADXRS450_Gyro m_gyro = new ADXRS450_Gyro();
+    private static final BuiltInAccelerometer m_accel = new BuiltInAccelerometer();
+    private static final ADIS16448_IMU m_imu = new ADIS16448_IMU();
 
     public Sensors() {
-        m_gyro = new ADXRS450_Gyro();
-        m_accel = new BuiltInAccelerometer();
     }
 
     public double getAngle() {
